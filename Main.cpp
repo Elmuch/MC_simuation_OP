de2af0da1f4ba5aaaeba58d17f7952d14955bfd9
Elijah Gachuhi <elgachuhi@gmai.com>
2014-07-20 16:16:03 -0400

    adds initial send to client

--- Main.cpp
+++ Main.cpp
@@ -27,14 +27,17 @@ int main()
     EuropeanOption Batch1(T,K,sig,r,S,b);
     CallOption C(Batch1);
     PutOption  P(Batch1);
+    ofstream SendtoClient;
+    SendtoClient.open("/home/ngwachi/Desktop/developer/results.json");
+        SendtoClient <<"Hello some output here"<<endl;
 
-    cout<<"---------------Batch1--------------------------------- \n"<<endl;
+    SendtoClient<<"---------------Batch1--------------------------------- \n"<<endl;
 
     //test  pricers
-    cout <<"Call price: " <<C.Price(Batch1)<<", Put Price: " <<P.Price(Batch1)<<endl;
-    cout<<"based on put-call parity: " <<C.Price_par(P.Price(Batch1)) //call price given Put price
+    SendtoClient <<"Call price: " <<C.Price(Batch1)<<", Put Price: " <<P.Price(Batch1)<<endl;
+    SendtoClient<<"based on put-call parity: " <<C.Price_par(P.Price(Batch1)) //call price given Put price
     <<" , "<<P.Price_par(C.Price(Batch1))<<endl;        //put price given call price
-    cout<< "Price given sig or T: Call: "<<C.Price(C,sig)<<" , Put: "<<P.Price(P,T)<<"\n"<<endl;    //price given volatility or maturity time
+    SendtoClient<< "Price given sig or T: Call: "<<C.Price(C,sig)<<" , Put: "<<P.Price(P,T)<<"\n"<<endl;    //price given volatility or maturity time
 
         //Test meshers
     vector<double> P_m= P.Price(Batch1,Mesh(40,60,10)); //put price mesher
@@ -50,7 +53,7 @@ int main()
     print(C_m1,string("Call price mesher given sig : ")); //
 
 
-cout<<"\n---------------Batch2--------------------------------- \n"<<endl;
+SendtoClient<<"\n---------------Batch2--------------------------------- \n"<<endl;
 
         //Batch 2
     T=1.0; K=100; sig =0.2; r=0.0; S= 100; b=0.0;
@@ -59,10 +62,10 @@ cout<<"\n---------------Batch2--------------------------------- \n"<<endl;
     PutOption  P2(Batch2);
 
    //test  pricers
-    cout <<"Call price: " <<C2.Price(Batch2)<<", Put Price: " <<P2.Price(Batch2)<<endl;
-    cout<<"based on put-call parity: " <<C2.Price_par(P2.Price(Batch2)) //call price given Put price
+    SendtoClient <<"Call price: " <<C2.Price(Batch2)<<", Put Price: " <<P2.Price(Batch2)<<endl;
+    SendtoClient<<"based on put-call parity: " <<C2.Price_par(P2.Price(Batch2)) //call price given Put price
     <<" , "<<P2.Price_par(C2.Price(Batch2))<<endl;        //put price given call price
-    cout<< "Price given sig or T: Call: "<<C2.Price(C2,sig)<<" , Put: "<<P2.Price(P,T)<<endl;    //price given volatility or maturity time
+    SendtoClient<< "Price given sig or T: Call: "<<C2.Price(C2,sig)<<" , Put: "<<P2.Price(P,T)<<endl;    //price given volatility or maturity time
 
     //Test meshers
     P_m= P2.Price(Batch2,Mesh(70,100,10)); //put price mesher
@@ -77,7 +80,7 @@ cout<<"\n---------------Batch2--------------------------------- \n"<<endl;
     C_m1= C2.Price(Mesh(70,100,10),sig); //call mesher given  volatiltiy
     print(C_m1,string("Call price mesher given sig ")); // call price on a range given sig or T
 
-cout<<"\n---------------Batch3--------------------------------- \n"<<endl;
+SendtoClient<<"\n---------------Batch3--------------------------------- \n"<<endl;
 
 //Batch 3
     T=1.0; K=10; sig =0.5; r=0.12; S= 5; b=r-0.0;
@@ -86,10 +89,10 @@ cout<<"\n---------------Batch3--------------------------------- \n"<<endl;
     PutOption  P3(Batch3);
 
    //test  pricers
-    cout <<"Call price: " <<C3.Price(Batch3)<<", Put Price: " <<P3.Price(Batch3)<<endl;
-    cout<<"based on put-call parity: " <<C3.Price_par(P3.Price(Batch3)) //call price given Put price
+    SendtoClient <<"Call price: " <<C3.Price(Batch3)<<", Put Price: " <<P3.Price(Batch3)<<endl;
+    SendtoClient<<"based on put-call parity: " <<C3.Price_par(P3.Price(Batch3)) //call price given Put price
     <<" , "<<P3.Price_par(C3.Price(Batch3))<<endl;        //put price given call price
-    cout<< "Price given sig or T: Call: "<<C3.Price(C3,sig)<<" , Put: "<<P3.Price(P,T)<<endl;    //price given volatility or maturity time
+    SendtoClient<< "Price given sig or T: Call: "<<C3.Price(C3,sig)<<" , Put: "<<P3.Price(P,T)<<endl;    //price given volatility or maturity time
 
     //Test meshers
     P_m= P3.Price(Batch3,Mesh(1,5,1)); //put price mesher
@@ -105,7 +108,7 @@ cout<<"\n---------------Batch3--------------------------------- \n"<<endl;
     print(C_m1,string("Call price mesher given sig ")); //
 
 
-cout<<"\n---------------Batch4--------------------------------- \n"<<endl;
+SendtoClient<<"\n---------------Batch4--------------------------------- \n"<<endl;
 
 //Batch 4
     T=30.0; K=100.0; sig =0.30; r=0.08; S= 100; b=r-0.0;
@@ -113,10 +116,10 @@ cout<<"\n---------------Batch4--------------------------------- \n"<<endl;
     CallOption C4(Batch4);
     PutOption  P4(Batch4);
 
-    cout <<"Call price: " <<C4.Price(Batch4)<<", Put Price: " <<P4.Price(Batch4)<<endl;
-    cout<<"based on put-call parity: " <<C4.Price_par(P4.Price(Batch4)) //call price given Put price
+    SendtoClient <<"Call price: " <<C4.Price(Batch4)<<", Put Price: " <<P4.Price(Batch4)<<endl;
+    SendtoClient<<"based on put-call parity: " <<C4.Price_par(P4.Price(Batch4)) //call price given Put price
     <<" , "<<P4.Price_par(C4.Price(Batch4))<<endl;        //put price given call price
-    cout<< "Price given sig or T: Call: "<<C4.Price(C4,sig)<<" , Put: "<<P4.Price(P,T)<<endl;    //price given volatility or maturity time
+    SendtoClient<< "Price given sig or T: Call: "<<C4.Price(C4,sig)<<" , Put: "<<P4.Price(P,T)<<endl;    //price given volatility or maturity time
 
     //Test meshers
     P_m= P4.Price(Batch4,Mesh(70,100,10)); //put price mesher
@@ -131,14 +134,14 @@ cout<<"\n---------------Batch4--------------------------------- \n"<<endl;
     C_m1= C4.Price(Mesh(70,100,10),sig); //call mesher given  volatiltiy
 
 //Test Greeks
-cout<<"\n------------------Greeks---------------------------" <<endl;
+SendtoClient<<"\n------------------Greeks---------------------------" <<endl;
     S=105; T=0.5;  r=0.1;  b=0.0; sig =0.36; K=100;
     EuropeanOption greek(T,K,sig,r,S,b);
     CallOption Call(greek);
     PutOption Put(greek);
 
      //Test delta
-    cout<<"Call delta: "<<Call.CallDelta(S)<<" Putdelta:  "<<Put.PutDelta(S)<<endl;
+    SendtoClient<<"Call delta: "<<Call.CallDelta(S)<<" Putdelta:  "<<Put.PutDelta(S)<<endl;
 
     //test meshers
     vector<double> Call_del= Call.CallDelta(Mesh(95,105,5)); //Call delta mesher
@@ -148,7 +151,7 @@ cout<<"\n------------------Greeks---------------------------" <<endl;
     print(Put_del, string ("Put delta mesher : "));
 
     //test gamma
-    cout <<"\n option gamma: "<<Put.Putgamma(S)<<endl;
+    SendtoClient <<"\n option gamma: "<<Put.Putgamma(S)<<endl;
 
     //gamma mesher
     vector<double> Put_gamma =Put.Putgamma(Mesh(95,105,5)); //put gamma mesher
@@ -160,14 +163,15 @@ cout<<"\n------------------Greeks---------------------------" <<endl;
 
 
     // Test appromation of delta and  gamma
-    cout<<"\n Approximation of sensitivities "<<endl;
-for (double h = 1.0; h>0.1; h-=0.1) //test convergence from 1.0 to 0.1
+    SendtoClient<<"\n Approximation of sensitivities "<<endl;
+for (double h = 2.0; h>0.1; h-=0.1) //test convergence from 1.0 to 0.1
 {
-    cout<<"h :"<<h<<" CallDelta: "<<Call.CallDelta_approx(h) //test call delta approximation
+    SendtoClient<<"h :"<<h<<" CallDelta: "<<Call.CallDelta_approx(h) //test call delta approximation
     <<" put delta: "<<Put.PutDelta_approx(h)    //test put delta approximation
     <<" Gamma: "<<Call.CallGamma_approx(h,Put)  //test gamma approximation
     <<endl;
 }
+SendtoClient.close();
 	return 0;
 } //end main
 
